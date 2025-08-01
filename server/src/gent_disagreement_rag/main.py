c6c7c6339f1d5102c25c0bc447d50c67f91f0998
Joe Webster <joseph.scott.webster@gmail.com>
2025-08-01 13:42:55 -0400

    Add CORS middleware

--- server/src/gent_disagreement_rag/main.py
+++ server/src/gent_disagreement_rag/main.py
@@ -3,6 +3,14 @@ from .core import RAGService
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def read_root():
